2578ea6df9546095b1cdc803660f9c15dd3612b8
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-06-11 17:22:18 -0400

    convert make shell to lf

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
-lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o src/hw-test.o src/hw-test.c
-lcc -Wa-l -Wl-m -Wl-j -Wm-yn"HW-TEST" -DUSE_SFR_FOR_REG -o hw-test.gb src/hw-test.o
+lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c ./src/hw-test.c
+lcc -Wa-l -Wl-m -Wl-j -Wm-yn"HW-TEST" -DUSE_SFR_FOR_REG -o hw-test.gb ./src/hw-test.o
